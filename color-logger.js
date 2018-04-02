189b7232b13e27419c1183bbfd19762bc2e235d1
DentFuse <shivam.dwi253@gmail.com>
2018-04-02 19:14:15 +0530

    Fix code, add level logging

--- color-logger.js
+++ color-logger.js
@@ -5,37 +5,37 @@ function log(cmessage){
   console.log(chalk.green("-- " + moment(moment()).format("DD MMM YYYY hh:mm:ss a") + " -- ") + cmessage);
 }
 
-function error(message) {
-  log(chalk.rgb(255, 0, 0)(message));
+exports.error = function error(message) {
+  log(chalk.rgb(255, 0, 0)("[ERROR] " + message));
 }
 
-function info(message) {
-  log(chalk.rgb(43, 0, 255)(message));
+exports.info = function info(message) {
+  log(chalk.rgb(0 , 0, 255)("[INFO] " + message));
 }
 
-function warn(message) {
-  log(chalk.rgb(255, 180, 51)(message));
+exports.warn = function warn(message) {
+  log(chalk.rgb(255, 180, 51)("[WARN] " + message));
 }
 
-function debug(message) {
-  log(chalk.rgb(124, 8, 255)(message));
+exports.debug = function debug(message) {
+  log(chalk.rgb(124, 8, 255)("[DEBUG] " + message));
 }
 
-function silly(message) {
-  log(chalk.rgb(66, 238, 255)(message));
+exports.silly = function silly(message) {
+  log(chalk.rgb(66, 238, 255)("[SILLY] " + message));
 }
 
-function rgb(r,g,b,message){
+exports.rgb = function rgb(r,g,b,message){
   log(chalk.rgb(r, g, b)(message));
 }
 
-function hex(hex,message) {
+exports.hex = function hex(hex,message) {
   log(chalk.hex(hex)(message));
 }
-error("Should log current time and print this in red");
-info("Should log current time and print this in blue");
-warn("Orange");
-debug("caught your eye");
-silly("Low level");
-rgb(247,28,251,"pink");
-hex("#0eff2d","green");
+// error("Should log current time and print this in red");
+// info("Should log current time and print this in blue");
+// warn("Orange");
+// debug("caught your eye");
+// silly("Low level");
+// rgb(247,28,251,"pink");
+// hex("#0eff2d","green");
